c1aa4f7d13016c915bef839ca4fecd239200a2ca
Andrej <rooney.andrej@gmail.com>
2020-06-28 10:48:25 +0200

    Jumping added, Death conditions added.

---
+++ Assets/Scripts/DragonKiller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DragonKiller : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y > 6)
+        {
+            // Load the scene at index 0
+            SceneManager.LoadScene(0);
+        }
+
+        if (transform.position.y < -6)
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
